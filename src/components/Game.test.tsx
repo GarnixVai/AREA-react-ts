accac8fae8541e3ede3394aa59761e8feb0414da
GarnixJu <ambitious_816@hotmail.com>
2021-08-13 00:43:06 +0200

    add test

--- src/components/Game.test.tsx
+++ src/components/Game.test.tsx
@@ -1,24 +1,30 @@
 import { render, screen, fireEvent, waitForElement, act } from '@testing-library/react';
 import { useEffect, useState } from "react";
+import { unmountComponentAtNode } from "react-dom";
 import '@testing-library/jest-dom';
 import Enzyme, { shallow, mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Game from './Game';
 configure({adapter: new Adapter()});
 let container: any = null;
-
+let initial:any; 
 // https://www.toptal.com/react/testing-react-hooks-tutorial
 
 describe('Game', function () {
+  const setState = jest.fn();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const useStateMock: any = (initState: any) => [initState, setState];
   beforeEach(() => {
     container = document.createElement("div");
     document.body.appendChild(container);
+    jest.useFakeTimers();
   });
   afterEach(() => {
     // cleanup on exiting
-    // unmountComponentAtNode(container);
+    unmountComponentAtNode(container);
     container.remove();
     container = null;
+    jest.useRealTimers();
   });
 
   it('renders game', () => {
@@ -26,13 +32,23 @@ describe('Game', function () {
     expect(container).toBeTruthy;
   });
 
-  it('test', ()=>{
-      render(<Game />);
-      // fireEvent.change(screen.getByC);
-      act(() => {
-        render(<Game />, container);
-      });
-      // expect(container.textContent).toBe("loading");
+  it('check initial state', ()=>{
+    const onChange = jest.fn();
+    act(() => {
+      render(<Game mode={false} score={0} userChoice={"scissors"} setScore={onChange} />, container);
+    });
+    const information = document.getElementsByTagName("pre")[0];
+    expect(information.textContent).toContain("Your Choice:scissors");
+
+  });
+
+  it('game rule case #1', ()=>{
+    const onChange = jest.fn();
+    act(() => {
+      render(<Game mode={false} score={0} userChoice={"scissors"} setScore={onChange} />, container);
+    });
+    const information = document.getElementsByTagName("pre")[0];
+    expect(information.textContent).toContain("Your Choice:scissors");
 
   });
 
